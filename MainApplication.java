02ac4e4d2ffd6d8d4205fffe321e31722aebb969
Valleru Sai Ramya <valleruramya19@gmail.com>
2024-05-05 19:25:54 -0500

    Added trade window

--- MainApplication.java
+++ MainApplication.java
@@ -7,6 +7,11 @@ import bookstore.ribbonLayout.RibbonLayout;
 
 public class MainApplication {
     public static void main(String[] args) {
-         SwingUtilities.invokeLater(LoginWindow::new);
+        //  SwingUtilities.invokeLater(LoginWindow::new);
+        SwingUtilities.invokeLater(() -> {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.dispose(); // Close the login window
+            new RibbonLayout(); // Open the main application window
+        });
     }
 }
